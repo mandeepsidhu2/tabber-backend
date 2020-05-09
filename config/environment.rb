cb08b27699211116873b6458936847e32ca95849
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-05-10 05:05:01 +0530

    added logger in env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-#Rails.logger = Logger.new(STDOUT)
-#config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+Rails.logger = Logger.new(STDOUT)
+config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
 # Initialize the Rails application.
 Rails.application.initialize!

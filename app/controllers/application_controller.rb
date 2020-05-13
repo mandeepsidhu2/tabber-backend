e4ffe26b7b3ec0073274ed3dd1f0bf16c39eb371
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-05-14 03:49:06 +0530

    correct date format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 require 'json_web_token'
 class ApplicationController < ActionController::API
 	  include ActionController::HttpAuthentication::Token::ControllerMethods
-	  
+
 	def set_api_current_user
 		@date=Date.today.in_time_zone('Kolkata').strftime("%Y-%m-%d").to_s;
 		header=request.headers["token"]
-		 puts("token is ",header)
+		 puts("date is ",@date)
 		begin
 		 @decoded = JsonWebToken.decode(header)
 		 @api_current_user = User.find(@decoded[:user_id])
